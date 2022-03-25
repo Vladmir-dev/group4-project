42dd92463d01290b4026ddb6abd7c44d1f5c381b
Vladmir-dev <tempestpius70@gmail.com>
2022-03-25 07:58:52 +0300

    added weather view

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import NewsAPIView
+from .views import NewsAPIView,WeatherAPIView
 from django.urls import re_path as url
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title="newsfeed api")
 
 urlpatterns = [
-    url(r'^$', schema_view),
+    # url(r'^$', schema_view),
     path('news/', NewsAPIView.as_view(), name="news"),
+    path('weather/', WeatherAPIView.as_view(), name="weather"),
 ]
--- api/views.py
+++ api/views.py
@@ -25,3 +25,22 @@ class NewsAPIView(GenericAPIView):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class WeatherAPIView(GenericAPIView):
+	serializer_class = WeatherSerializer
+	queryset = Weather.objects.all()
+
+	def get(self, request, format=None):
+		
+		serializer = self.serializer_class(self.get_queryset(), many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = self.serializer_class(data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
